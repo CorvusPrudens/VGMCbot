392eeb5dac71aace863e3a0c8f91e35c1762d28c
CorvusPrudens <corvusprudens@gmail.com>
2020-12-13 15:34:27 -0700

    enemy brainstorming

---
+++ src/games/rogue/enemies.py
@@ -0,0 +1,19 @@
+
+enemyTemplate = {
+    'name': 'sentry bot',
+    'x': 0,
+    'y':0,
+    'hp': 10.0,
+    'speed': 0.5, # units/sec
+    'inventory': [
+        {'name': 'diagnostic lens',
+         'desc': 'this lens grants insight',
+         'type': 'active', }
+    ]
+    #'attacks' these should come from inv items
+    #'armor' this as well
+    'stats': { #base stats
+        'strength': 0,
+        ''
+    }
+}
