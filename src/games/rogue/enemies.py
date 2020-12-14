fc4124be0fac0ef58e33d955a29cdef238f17ff1
CorvusPrudens <corvusprudens@gmail.com>
2020-12-13 22:50:52 -0700

    more structure

--- src/games/rogue/enemies.py
+++ src/games/rogue/enemies.py
@@ -1,19 +1,46 @@
 
+
+def diagMethod():
+    pass
+    
+item1 = {
+    'name': 'diagnostic lens',
+    'desc': 'this lens grants insight',
+    'type': 'active',
+    'moveset': [
+        {
+            'name': 'analyze',
+            'targets': 1,
+            'method': diagMethod,
+        }
+    ]
+    'stat bonus': {}
+}
+
 enemyTemplate = {
     'name': 'sentry bot',
     'x': 0,
     'y':0,
     'hp': 10.0,
-    'speed': 0.5, # units/sec
     'inventory': [
-        {'name': 'diagnostic lens',
-         'desc': 'this lens grants insight',
-         'type': 'active', }
+        {
+            'name': 'diagnostic lens',
+            'desc': 'this lens grants insight',
+            'type': 'active',
+        },
+        {
+            'name': 'stun module',
+        },
     ]
     #'attacks' these should come from inv items
     #'armor' this as well
     'stats': { #base stats
         'strength': 0,
-        ''
+        'tech': 5,
+        'speed': 0.5, # units / sec
+        'heat resistance': 5,
+        'physical resistance': 2,
+        'electrical resistance': 1,
+        'radiation resistance': 3,
     }
 }
